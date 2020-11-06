f5a6d496e999c5ecfbb072485f4d6e337348a3a5
Louis Dalligos <zindex14@gmail.com>
2020-11-06 20:38:49 +0800

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { AppProps } from 'next/app';
+import { useRouter } from "next/router";
+import * as gtag from "@utils/gtag";
 //import { PropertyProvider } from '@context/property/Property';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -6,5 +9,17 @@ import '@styles/accordion.css';
 import '@styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
